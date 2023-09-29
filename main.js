a2c5c07a2f6541dda0f4c40be1328b55e6def353
saracatpr <saracpr99@gmail.com>
2023-09-28 21:35:50 -0500

    segunda preentrega, está incompleta

---
+++ main.js
@@ -0,0 +1,22 @@
+alert()
+
+let watchlist = [
+    {title: "Cheer Up",
+    genre: "drama",
+    year: 2022},
+    {title: "Alchemy of Souls",
+    genre: "fantasy",
+    year: 2022},
+    {title: "Soundtrak #1",
+    genre: "romance",
+    year: 2022},
+    {title: "Goblin",
+    genre: "fantasy",
+    year: 2016},
+    {title: "Crash Landing On You",
+    genre: "drama",
+    year: 2019},
+    {title: "Doom At Your Service",
+    genre: "fantasy",
+    year: 2021}
+]
