35da3c68495b4d959dbb8939436c2b4f2a6b4bd8
saracatpr <saracpr99@gmail.com>
2023-10-10 23:26:44 -0500

    movie watchlist 1

--- main.js
+++ main.js
@@ -1,22 +1,52 @@
-alert()
+const inputBox = document.getElementById("input-box")
 
-let watchlist = [
-    {title: "Cheer Up",
-    genre: "drama",
-    year: 2022},
-    {title: "Alchemy of Souls",
-    genre: "fantasy",
-    year: 2022},
-    {title: "Soundtrak #1",
-    genre: "romance",
-    year: 2022},
-    {title: "Goblin",
-    genre: "fantasy",
-    year: 2016},
-    {title: "Crash Landing On You",
-    genre: "drama",
-    year: 2019},
-    {title: "Doom At Your Service",
-    genre: "fantasy",
-    year: 2021}
-]
+const listContainer = document.getElementById("list-container")
+
+function addMovie(){
+    if(inputBox.value === ""){
+        alert("Please insert a movie or show you want to watch!")
+    }else{
+        let li= document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+    inputBox.value = ""
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "li"){
+        e.target.classList.toggle("checked")
+    }
+}, false);
+
+
+
+// let watchlist = ["Ballerina", "Bottoms", "Suzume"];
+
+
+// let listItems = document.querySelectorAll('li');
+
+// for (let i = 0; i < listItems.length; i++) {
+//     console.log(listItems[i].textContent);
+// }
+
+// for (let i = 0; i < watchlist.length; i++) {
+//     console.log(watchlist[i]);
+// }
+
+// let liArray = [];
+// let liElements = document.querySelectorAll("li");
+// for (let i=0; i<liElements.length; i++){
+//     liArray.push(liElements[i]);
+// }
+
+
+//  const ul = document.querySelector('ul');
+// const lis = ul.querySelectorAll('li');
+// const array = [];
+
+// lis.forEach((li) => {
+//     array.push(li.textContent);
+// });
+
+// console.log(array); 
